43805756e5fac146188ad8afeb74294ebff14e40
lojhbm <lojhbm@gmail.com>
2026-08-07 22:45:21 +0900

    chore: 개발 환경 초기 세팅 완료

---
+++ src/test/java/com/walktowall/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walktowall.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
