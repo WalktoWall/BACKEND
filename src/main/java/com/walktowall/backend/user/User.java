11fa836bb8e5f47be19797129cc38dc5b8854c8b
yunjin-grit <gracelee0730@naver.com>
2026-08-09 14:12:16 +0900

    feat: User Entity 및 Repository 구현

---
+++ src/main/java/com/walktowall/backend/user/User.java
@@ -0,0 +1,20 @@
+package com.walktowall.backend.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName; // user 이름
+}
