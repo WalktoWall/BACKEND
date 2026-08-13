aa96299cbad68825e909ea42ee880c9010bd6eb6
lojhbm <lojhbm@gmail.com>
2026-08-14 01:08:12 +0900

    feat: 스캔 상품 등록 기능 구현 완료

--- src/main/java/com/walktowall/backend/visitcard/VisitCardRepository.java
+++ src/main/java/com/walktowall/backend/visitcard/VisitCardRepository.java
@@ -2,5 +2,8 @@ package com.walktowall.backend.visitcard;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VisitCardRepository extends JpaRepository<VisitCard, Integer> {
+    Optional<VisitCard> findByUser_UserId(Integer userId);
 }
