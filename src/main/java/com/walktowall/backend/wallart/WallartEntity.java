35791de04c989276b694b9198fe2d0e96acadcfe
lojhbm <lojhbm@gmail.com>
2026-08-14 19:31:27 +0900

    feat: wallartEntity 클래스 생성 및 코드 작성

--- src/main/java/com/walktowall/backend/wallart/WallartEntity.java
+++ src/main/java/com/walktowall/backend/wallart/WallartEntity.java
@@ -1,4 +1,28 @@
 package com.walktowall.backend.wallart;
 
+import com.walktowall.backend.visitcard.VisitCard;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "wallart")
 public class WallartEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wallart_id")
+    private Integer wallartId;
+
+    @OneToOne
+    @JoinColumn(name = "visit_card_id", nullable = false)
+    private VisitCard visitCard;
+
+    @Column(name = "wallart_img", nullable = false)
+    private String wallartImg;
 }
