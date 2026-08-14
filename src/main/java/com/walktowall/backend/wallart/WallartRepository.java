17fcb67487811eb914d88c6169f131c84a264cf1
lojhbm <lojhbm@gmail.com>
2026-08-14 18:52:19 +0900

    feat: wallart 패키지 기본 세팅

---
+++ src/main/java/com/walktowall/backend/wallart/WallartRepository.java
@@ -0,0 +1,4 @@
+package com.walktowall.backend.wallart;
+
+public interface WallartRepository {
+}
---
+++ src/main/java/com/walktowall/backend/wallart/dto/CreateWallartResponse.java
@@ -0,0 +1,4 @@
+package com.walktowall.backend.wallart.dto;
+
+public class CreateWallartResponse {
+}
